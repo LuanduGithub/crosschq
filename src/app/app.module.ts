41ae8fb91acd22eb50c24bf643f99e43776f9cc4
Luis Dubrez <luis.dubrez@gmail.com>
2022-05-16 13:06:06 -0300

    fix(add entry component)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -53,6 +53,7 @@ import { CommonModule } from '@angular/common';
     MatDialogModule
   ],
   providers: [],
+  entryComponents: [AddDialogComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
